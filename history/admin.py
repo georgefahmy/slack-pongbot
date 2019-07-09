c335485f47046ccb41f871be267523604ef22cb7
George Fahmy <geofahm@gmail.com>
2019-07-09 15:51:13 -0700

    Rankings db testing (#2)
    * clean up some code
    
    * fixed admin.py, removed unneccesary auto generated file

--- history/admin.py
+++ history/admin.py
@@ -3,4 +3,3 @@ from django.contrib import admin
 from .models import Game
 
 admin.site.register(Game)
-admin.site.register(Rankings)
--- history/migrations/0004_season_season_number.py
+++ history/migrations/0004_season_season_number.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('history', '0002_season'),
+        ('history', '0003_rankings'),
     ]
 
     operations = [
